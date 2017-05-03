ed06918e771177e59f32aae788fc70e66f4b56cb
Ryan Adolf <ryanadolf123@gmail.com>
2017-05-02 19:20:20 -0700

    Implement NetConsole functionality

--- patch/wpilibj/src/athena/java/edu/wpi/first/wpilibj/hal/frccom/ComServer.java
+++ patch/wpilibj/src/athena/java/edu/wpi/first/wpilibj/hal/frccom/ComServer.java
@@ -1,10 +1,17 @@
 package edu.wpi.first.wpilibj.hal.frccom;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintStream;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.InetAddress;
 import java.net.SocketException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.atomic.AtomicBoolean;
+
 import edu.wpi.first.wpilibj.hal.frccom.DsToRobot;
 import edu.wpi.first.wpilibj.hal.frccom.RobotToDs;
 
@@ -14,12 +21,13 @@ import edu.wpi.first.wpilibj.hal.frccom.RobotToDs;
 public class ComServer implements Runnable {
 
     private static final int RECV_PORT = 1110;
-
     private static final int RECV_PACKETSIZE = 100;
 
     private static final int SEND_PORT = 1150;
+    private static final int NETCONSOLE_PORT = 6666;
 
     private DatagramSocket socket;
+    private Map<InetAddress, Long> clientMap;
 
     // Volatile because this will be set from other threads
     private volatile boolean stop;
@@ -30,13 +38,16 @@ public class ComServer implements Runnable {
 
     public ComServer() throws SocketException {
         socket = new DatagramSocket(RECV_PORT);
+        clientMap = new HashMap<InetAddress, Long>();
     }
 
     public void run() {
+        startRedirectingStdout();
         while (!stop) {
             DatagramPacket packet = new DatagramPacket(new byte[RECV_PACKETSIZE], RECV_PACKETSIZE);
             try {
                 socket.receive(packet);
+                clientMap.put(packet.getAddress(), System.currentTimeMillis());
                 byte[] data = new byte[packet.getLength()];
                 System.arraycopy(packet.getData(), packet.getOffset(), data, 0, packet.getLength());
                 DsToRobot sent = DsToRobot.fromByteArray(data);
@@ -89,5 +100,55 @@ public class ComServer implements Runnable {
 
     public AtomicBoolean getLock() {
         return lock;
+    }
+
+    private void broadcast(byte[] buffer, int port) {
+        long t = System.currentTimeMillis();
+        for (InetAddress a : clientMap.keySet()) {
+            long timeDiff = t - clientMap.get(a);
+            // Only send to clients that have sent something in the last 100ms
+            if (timeDiff < 100) {
+                DatagramPacket sendPacket = new DatagramPacket(buffer, buffer.length, a, port);
+                try {
+                    socket.send(sendPacket);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                clientMap.remove(a);
+            }
+        }
+        try {
+            byte[] address = {127, 0, 0, 1};
+            DatagramPacket sendPacket = new DatagramPacket(buffer, buffer.length, InetAddress.getByAddress(address), port);
+            socket.send(sendPacket);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void startRedirectingStdout() {
+        PrintStream orgStream = System.out;
+        System.setOut(new PrintStream(new OutputStream() {
+            private ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+
+            @Override
+            public void write(int b) {
+                byte byt = (byte) b;
+                if (byt == '\n') {
+                    broadcast(buffer.toByteArray(), NETCONSOLE_PORT);
+                    buffer.reset();
+                } else if (byt != '\r') {
+                    buffer.write(b);
+                }
+                orgStream.write(b);
+            }
+
+            @Override
+            public void flush() throws IOException {
+                super.flush();
+                orgStream.flush();
+            }
+        }));
     }
 }
